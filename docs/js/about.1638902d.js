2d490244f566718951b9243a3cad10542b672988
Michael Howard <michow289@hotmail.com>
2021-08-24 22:59:44 +0100

    new build with source maps

--- docs/js/about.1638902d.js
+++ docs/js/about.1638902d.js
@@ -1,1 +1,2 @@
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(n,t,a){"use strict";a.r(t);var e=a("7a23"),c={class:"about"},o=Object(e["g"])("h1",null,"This is an about page",-1),s=[o];function u(n,t){return Object(e["r"])(),Object(e["f"])("div",c,s)}const b={};b.render=u;t["default"]=b}}]);
+//# sourceMappingURL=about.1638902d.js.map
